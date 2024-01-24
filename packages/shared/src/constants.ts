7430f18a118db3ba4753c0f0b731cb9ca5d91862
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2024-01-24 15:52:28 +0100

    chore: changed speed

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -1,15 +1,15 @@
 export const TPS = 60; // ticks per second
 
-export const BASE_SPEED = 5;
-export const SPRINT_SPEED = 10;
+export const BASE_SPEED = 2;
+export const SPRINT_SPEED = 5;
 
 /**
  * Enum for socket events.
  * @enum {string}
  */
 export enum SOCKET_EVENTS {
-  CONNECT = 'connect',
-  DISCONNECT = 'disconnect',
-  MOVE = 'move',
-  FRAME = 'frame',
+  CONNECT = "connect",
+  DISCONNECT = "disconnect",
+  MOVE = "move",
+  FRAME = "frame",
 }
