486d7990eaf3bd142ee0ad86dd37a39e32b60a32
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2024-01-30 19:46:24 +0100

    feat: metadata

--- packages/client/src/app/layout.tsx
+++ packages/client/src/app/layout.tsx
@@ -1,14 +1,31 @@
-"use client"
-
-import { SharedStateProvider } from '@/lib/shared-state';
+import { SharedStateProvider } from "@/lib/shared-state";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
+import { Metadata } from "next";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
+export const metadata: Metadata = {
+  title: "Viper Vortex",
+  description:
+    "Collect points by eating food and other players. If your head touches another player, you will explode and die.",
+  category: "Game",
+  applicationName: "Viper Vortex",
+  robots: "index, follow",
+  authors: [
+    {
+      name: "Mael KERICHARD",
+      url: "https://mael.app",
+    },
+    {
+      name: "Cody ADAM",
+      url: "https://codya.dev",
+    },
+  ],
+};
 
 export default function RootLayout({
   children,
@@ -17,7 +34,7 @@ export default function RootLayout({
 }) {
   return (
     <SharedStateProvider>
-      <html lang="en" className='h-full'>
+      <html lang="en" className="h-full">
         <body className={`h-full font-sans ${inter.variable}`}>{children}</body>
       </html>
     </SharedStateProvider>
